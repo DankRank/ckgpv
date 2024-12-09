20eab7e242d01e0b8ab4fe0421caa3e2c3d84710
Egor <egor@opensrc.club>
2024-12-09 11:25:27 +0200

    initial commit

---
+++ ckgpv.go
@@ -0,0 +1,85 @@
+// Copyright (c) 2024 Egor
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package ckgpv
+
+import (
+	"slices"
+	"strconv"
+	"strings"
+
+	"github.com/gocolly/colly/v2"
+)
+
+type Page struct {
+	Title string      `json:"title"`
+	Rows  [][2]string `json:"rows"`
+}
+type GPVs struct {
+	LastId int           `json:"lastId"`
+	Pages  map[int]*Page `json:"pages"`
+}
+
+func Update(gpvs *GPVs) {
+	minId := gpvs.LastId
+
+	newsCollector := colly.NewCollector()
+	newsCollector.OnHTML(":root", func(e *colly.HTMLElement) {
+		id, err := strconv.Atoi(strings.TrimPrefix(e.Request.URL.Path, "/news/"))
+		if err != nil {
+			panic(err)
+		}
+		tds := e.ChildTexts("td")
+		rows := make([][2]string, len(tds)/2)
+		for i := range len(tds) / 2 {
+			rows[i][0] = tds[2*i]
+			rows[i][1] = tds[2*i+1]
+		}
+		gpvs.Pages[id] = &Page{
+			Title: e.ChildText("title"),
+			Rows:  rows,
+		}
+	})
+
+	homepageCollector := colly.NewCollector()
+	homepageCollector.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		href := e.Attr("href")
+		if strings.HasPrefix(href, "/news/") {
+			id, err := strconv.Atoi(strings.TrimPrefix(href, "/news/"))
+			if err != nil {
+				panic(err)
+			}
+			if id > gpvs.LastId {
+				gpvs.LastId = id
+			}
+			if id > minId && strings.Contains(e.Text, "погодинних відключень") {
+				newsCollector.Visit(e.Request.AbsoluteURL(href))
+			}
+		}
+	})
+	homepageCollector.Visit("https://cherkasyoblenergo.com/")
+}
+func Filter(gpvs *GPVs, shard int) {
+	shardCh := strconv.Itoa(shard) // assume shard < 10
+	for _, page := range gpvs.Pages {
+		page.Rows = slices.DeleteFunc(page.Rows, func(row [2]string) bool {
+			return !strings.Contains(row[1], shardCh)
+		})
+	}
+}
+func Summarize(page *Page) string {
+	fromto := make([][2]string, 0, len(page.Rows))
+	for _, row := range page.Rows {
+		s := strings.Split(row[0], "-")
+		if len(fromto) > 0 && fromto[len(fromto)-1][1] == s[0] {
+			fromto[len(fromto)-1][1] = s[1]
+		} else {
+			fromto = append(fromto, [2]string(s))
+		}
+	}
+	summary := page.Title
+	for _, s := range fromto {
+		summary += "\n" + s[0] + "-" + s[1]
+	}
+	return summary
+}
---
+++ cmd/ckgpv/main.go
@@ -0,0 +1,100 @@
+// Copyright (c) 2024 Egor
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/DankRank/ckgpv"
+
+	"github.com/gorilla/feeds"
+)
+
+func main() {
+	var minId, shard int
+	var doFeed, dryRun bool
+	var listenAddr string
+	flag.IntVar(&minId, "min-id", 0, "starting page id")
+	flag.IntVar(&shard, "shard", 0, "shard number")
+	flag.BoolVar(&doFeed, "feed", false, "run a webserver with an atom feed")
+	flag.StringVar(&listenAddr, "listen-addr", ":8091", "(feed) listen address/port")
+	flag.BoolVar(&dryRun, "dry-run", false, "(feed) don't keep state")
+	flag.Parse()
+
+	if !doFeed {
+		gpvs := ckgpv.GPVs{LastId: minId, Pages: make(map[int]*ckgpv.Page)}
+		ckgpv.Update(&gpvs)
+		if shard > 0 {
+			ckgpv.Filter(&gpvs, shard)
+		}
+
+		bytes, err := json.Marshal(&gpvs)
+		if err != nil {
+			panic(err)
+		}
+		os.Stdout.Write(bytes)
+	} else {
+		dummyTimestamp := time.Now()
+		gpvs := ckgpv.GPVs{LastId: minId, Pages: make(map[int]*ckgpv.Page)}
+
+		bytes, err := os.ReadFile("ckgpv-state.json")
+		if err == nil {
+			err = json.Unmarshal(bytes, &gpvs)
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		http.HandleFunc("/feed.xml", func(w http.ResponseWriter, _ *http.Request) {
+			ckgpv.Update(&gpvs)
+			if shard > 0 {
+				ckgpv.Filter(&gpvs, shard)
+			}
+
+			feed := &feeds.Feed{
+				Id:      "https://cherkasyoblenergo.com/",
+				Title:   "Cherkasy GPV",
+				Updated: dummyTimestamp,
+				Link:    &feeds.Link{Href: "https://cherkasyoblenergo.com/"},
+			}
+			feed.Items = make([]*feeds.Item, 0, len(gpvs.Pages))
+			for i, page := range gpvs.Pages {
+				id := "https://cherkasyoblenergo.com/news/" + strconv.Itoa(i)
+				feed.Items = append(feed.Items,
+					&feeds.Item{
+						Id:      id,
+						Title:   ckgpv.Summarize(page),
+						Updated: dummyTimestamp,
+						Link:    &feeds.Link{Href: id, Type: "text/html"},
+					})
+			}
+			atom, err := feed.ToAtom()
+			if err != nil {
+				panic(err)
+			}
+			io.WriteString(w, atom)
+
+			if len(gpvs.Pages) > 0 && !dryRun {
+				// For my uses, I don't need to keep these after they've been retrieved by the client
+				clear(gpvs.Pages)
+
+				bytes, err := json.Marshal(&gpvs)
+				if err != nil {
+					panic(err)
+				}
+				err = os.WriteFile("ckgpv-state.json", bytes, 0666)
+				if err != nil {
+					panic(err)
+				}
+			}
+		})
+		http.ListenAndServe(listenAddr, nil)
+	}
+}
